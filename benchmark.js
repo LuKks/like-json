a5b13eee80ed9ad8d9c04e06acf19e85a8538f25
Daniel Sokil <dtechtours@gmail.com>
2019-07-07 20:55:55 -0400

    Add Benchmark Suite

---
+++ benchmark.js
@@ -0,0 +1,41 @@
+const Benchmark = require("benchmark");
+const like = require("./index.js");
+
+const suite = new Benchmark.Suite();
+
+let data = {
+  msg: "Your link was created.",
+  ok: true,
+  data: { link: ["3b673d", 4882794] }
+};
+
+let stringify = like.json(data);
+
+suite
+  .add("JSON.stringify", function() {
+    JSON.stringify(data);
+  })
+  .add("like.stringify", function() {
+    like.stringify(data, 123);
+  })
+  .add("like.json", function() {
+    stringify(data);
+  })
+  .add("concatenation", function() {
+    '{"msg":"' +
+      data.msg +
+      '","ok":' +
+      data.ok +
+      ',"data":{"link":["' +
+      data.data.link[0] +
+      '",' +
+      data.data.link[1] +
+      "]}}";
+  })
+  .on("cycle", function(event) {
+    console.log(String(event.target));
+  })
+  .on("complete", function() {
+    console.log("Fastest is " + this.filter("fastest").map("name"));
+  })
+  .run({ async: true });
